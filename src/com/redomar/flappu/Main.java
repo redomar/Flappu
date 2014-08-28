fa24f24699722bf46f578c5fb841079ce4794d8b
Mohamed Omar <mohamed12@live.co.uk>
2014-08-28 02:31:21 +0100

    Added comments to the code

--- src/com/redomar/flappu/Main.java
+++ src/com/redomar/flappu/Main.java
@@ -1,11 +1,14 @@
 package com.redomar.flappu;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.PixelFormat;
 
 public class Main implements Runnable{
 
+	//Setting private variables such as screen size etc...
 	private int width = 854;
 	private int height = 480;
 	private String title = "Flappu";
@@ -21,24 +24,41 @@ public class Main implements Runnable{
 
 	public void run(){
 		try {
+			// Sets the display frame to the size and names the window to Flappu.
 			Display.setDisplayMode(new DisplayMode(width, height));
 			Display.setTitle(title);
-			Display.create();
+			
+			/*
+			 * Sets OpenGL version to 3.3
+			 * There is a bug in LWJGL 2.9.1 where context(3, 3)
+			 * in Mac will run OpenGL 2.1, So here I set context(3, 2)
+			 * and should run the OpenGL 3.3+ on Mac.
+			 */
+			ContextAttribs context = new ContextAttribs(3, 3);
+			if(System.getProperty("os.name").contains("Mac")){
+				context = new ContextAttribs(3, 2);
+			}
+			
+			// Loads the display to memory and opens it in a window.
+			Display.create(new PixelFormat(), context.withProfileCore(true));
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
 		
 		while(running){
+			// Updates the display every tick.
 			Display.update();
 			if(Display.isCloseRequested()){
 				running = false;
 			}
 		}
 		
+		// Unloads the display from memory and closes the window.
 		Display.destroy();
 	}
 
 	public static void main(String[] args) {
+		// Starts a thread in the start() class and sets runnable to true.
 		new Main().start();
 	}
 
