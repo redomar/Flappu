945fa23021babf530767fd7c9b2b105302c5f23f
Mohamed Omar <mohamed12@live.co.uk>
2014-08-29 21:16:15 +0100

    Added vector and matrix classes

--- src/com/redomar/flappu/Main.java
+++ src/com/redomar/flappu/Main.java
@@ -8,74 +8,73 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.PixelFormat;
 
-public class Main implements Runnable{
+public class Main implements Runnable {
 
-	//Setting private variables such as screen size etc...
+	// Setting private variables such as screen size etc...
 	private int width = 854;
 	private int height = 480;
 	private String title = "Flappu";
 
 	private boolean running = false;
 	private Thread thread;
-	
+
 	/**
 	 * @author Redomar
 	 */
-	
-	private void init(){
-		String version = glGetString(GL_VERSION); //This will request OpenGL version as a string.
-		System.out.println("OpenGL: "+ version);
-		
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //This sets clear colour to white.
+
+	private void init() {
+		String version = glGetString(GL_VERSION); // This will request OpenGL version as a string.
+		System.out.println("OpenGL: " + version);
+
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // This sets clear colour to white.
 	}
 
-	public void start(){
+	public void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
 
-	public void run(){
+	public void run() {
 		try {
 			// Sets the display frame to the size and names the window to Flappu.
 			Display.setDisplayMode(new DisplayMode(width, height));
 			Display.setTitle(title);
-			
+
 			/*
-			 * Sets OpenGL version to 3.3
-			 * There is a bug in LWJGL 2.9.1 where context(3, 3)
-			 * in Mac will run OpenGL 2.1, So here I set context(3, 2)
-			 * and should run the OpenGL 3.3+ on Mac.
+			 * Sets OpenGL version to 3.3 There is a bug in LWJGL 2.9.1 where
+			 * context(3, 3) in Mac will run OpenGL 2.1, So here I set
+			 * context(3, 2) and should run the OpenGL 3.3+ on Mac.
 			 */
 			ContextAttribs context = new ContextAttribs(3, 3);
-			if(System.getProperty("os.name").contains("Mac")){
+			if (System.getProperty("os.name").contains("Mac")) {
 				context = new ContextAttribs(3, 2);
 			}
-			
+
 			// Loads the display to memory and opens it in a window.
 			Display.create(new PixelFormat(), context.withProfileCore(true));
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
-		
+
 		init();
-		
-		while(running){
+
+		while (running) {
 			render();
-			
+
 			// Updates the display every tick.
 			Display.update();
-			if(Display.isCloseRequested()){
+			if (Display.isCloseRequested()) {
 				running = false;
 			}
 		}
-		
+
 		// Unloads the display from memory and closes the window.
 		Display.destroy();
 	}
-	
-	private void render(){
-		glClear(GL_COLOR_BUFFER_BIT); //This clears the screen to the clear colour we set as white.
+
+	private void render() {
+		glClear(GL_COLOR_BUFFER_BIT); // This clears the screen to the clear colour we set as white.
 	}
 
 	public static void main(String[] args) {
